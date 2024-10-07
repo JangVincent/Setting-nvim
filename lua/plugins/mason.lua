7c20e22e6b1e29f98e8203395607403d3940b66a
Vincent Jang <vincentjangdev@gmail.com>
2024-10-07 10:30:48 +0900

    fix : typescript ls

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -11,7 +11,7 @@ return {
     opts = {
       ensure_installed = {
         "lua_ls",
-        "tsserver",
+        "ts_ls",
         "jdtls"
         -- add more arguments for adding more language servers
       },
